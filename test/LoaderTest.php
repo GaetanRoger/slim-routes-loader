930d6a615e0f8defe1cd0e59c951cf3c11e93995
Gaétan ROGER <gaetanroger.fr@gmail.com>
2017-11-25 14:19:15 +0100

    Now test all methods and cases when the HTTP method is unknown

--- test/LoaderTest.php
+++ test/LoaderTest.php
@@ -240,4 +240,160 @@ class LoaderTest extends AbstractLoaderTestCase
         $this->assertCount(1, $logger->debugs);
         $this->assertEquals('Registered new GET route: /', $logger->debugs[0]['message']);
     }
+    
+    public function testAllMethods()
+    {
+        $routes = [
+            'pattern' => '',
+            'routes'  => [
+                [
+                    'pattern'  => '/one',
+                    'method'   => 'GET',
+                    'callable' => 'testCallable1',
+                    'name'     => 'testName1',
+                ],
+                [
+                    'pattern'  => '/two',
+                    'method'   => 'POST',
+                    'callable' => 'testCallable2',
+                    'name'     => 'testName2',
+                ],
+                [
+                    'pattern'  => '/three',
+                    'method'   => 'PUT',
+                    'callable' => 'testCallable3',
+                    'name'     => 'testName3',
+                ],
+                [
+                    'pattern'  => '/four',
+                    'method'   => 'PATCH',
+                    'callable' => 'testCallable4',
+                    'name'     => 'testName4',
+                ],
+                [
+                    'pattern'  => '/five',
+                    'method'   => 'OPTIONS',
+                    'callable' => 'testCallable5',
+                    'name'     => 'testName5',
+                ],
+                [
+                    'pattern'  => '/six',
+                    'method'   => 'DELETE',
+                    'callable' => 'testCallable6',
+                    'name'     => 'testName6',
+                ],
+            ],
+        ];
+        
+        $loader = new Loader($routes);
+        $loader($this->slim);
+        
+        $r = $this->getRoutes();
+        
+        $this->assertNotEmpty($r);
+        $this->assertRoute(
+            $r['route0'],
+            1,
+            ['GET'],
+            'testName1',
+            'testCallable1',
+            '/one',
+            1,
+            ['']
+        );
+        $this->assertRoute(
+            $r['route1'],
+            1,
+            ['POST'],
+            'testName2',
+            'testCallable2',
+            '/two',
+            1,
+            ['']
+        );
+        $this->assertRoute(
+            $r['route2'],
+            1,
+            ['PUT'],
+            'testName3',
+            'testCallable3',
+            '/three',
+            1,
+            ['']
+        );
+        $this->assertRoute(
+            $r['route3'],
+            1,
+            ['PATCH'],
+            'testName4',
+            'testCallable4',
+            '/four',
+            1,
+            ['']
+        );
+        $this->assertRoute(
+            $r['route4'],
+            1,
+            ['OPTIONS'],
+            'testName5',
+            'testCallable5',
+            '/five',
+            1,
+            ['']
+        );
+        $this->assertRoute(
+            $r['route5'],
+            1,
+            ['DELETE'],
+            'testName6',
+            'testCallable6',
+            '/six',
+            1,
+            ['']
+        );
+        
+    }
+    
+    public function testUnknownHTTPMethod()
+    {
+        $routes = [
+            'pattern' => '',
+            'routes'  => [
+                [
+                    'pattern'  => '/',
+                    'method'   => 'UNKNOWN',
+                    'callable' => 'testCallable',
+                    'name'     => 'testName',
+                ],
+            ],
+        ];
+        
+        $logger = new Logger();
+        $loader = new Loader($routes, $logger);
+        $loader($this->slim);
+        
+        $this->assertCount(1, $logger->warnings);
+        $this->assertCount(0, $logger->errors);
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testUnknownHTTPMethodWithThrowing()
+    {
+        $routes = [
+            'pattern' => '',
+            'routes'  => [
+                [
+                    'pattern'  => '/',
+                    'method'   => 'UNKNOWN',
+                    'callable' => 'testCallable',
+                    'name'     => 'testName',
+                ],
+            ],
+        ];
+        
+        $loader = new Loader($routes);
+        $loader($this->slim, true);
+    }
 }
